7d72ddf1e1d0523e8f26fdfdb1a35d462d665349
lblake <treleven.lloyd@gmail.com>
2023-04-14 18:56:50 +0100

    added shuffle cards functionality

--- react-memory-game/src/App.js
+++ react-memory-game/src/App.js
@@ -1,12 +1,40 @@
-import './App.css'
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+import './App.css';
+
+const cardImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const unique_id = uuid();
+
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: unique_id }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
 
-export default App
+export default App;
