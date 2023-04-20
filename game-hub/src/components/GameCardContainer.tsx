bc18a3946264ff9210aa6f7ff6f5cdb619e44fb0
lblake <treleven.lloyd@gmail.com>
2023-04-20 08:38:26 +0100

    added code to display genres is side panel

--- game-hub/src/components/GameCardContainer.tsx
+++ game-hub/src/components/GameCardContainer.tsx
@@ -3,12 +3,11 @@ import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
- 
 }
 
-const GameCardContainer = ({ children}: Props) => {
+const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box  width='300px' borderRadius={10} overflow='hidden'>
+    <Box borderRadius={10} overflow='hidden'>
       {children}
     </Box>
   );
