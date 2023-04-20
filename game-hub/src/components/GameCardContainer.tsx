0ea34eb7bfa6eb0547313d4c6831835f7a8f6a7b
lblake <treleven.lloyd@gmail.com>
2023-04-20 08:16:21 +0100

    created genric data hook

--- game-hub/src/components/GameCardContainer.tsx
+++ game-hub/src/components/GameCardContainer.tsx
@@ -3,9 +3,10 @@ import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
+ 
 }
 
-const GameCardContainer = ({ children }: Props) => {
+const GameCardContainer = ({ children}: Props) => {
   return (
     <Box  width='300px' borderRadius={10} overflow='hidden'>
       {children}
